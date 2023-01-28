c657b091a7873f7841985f660745783df14a1347
s2011r2593 <seanrhee2024@u.northwestern.edu>
2023-01-28 15:24:02 -0600

    Possibly functional SoE calculation (that definitely should be review… (#89)
    * Possibly functional SoE calculation (that definitely should be reviewed tho)
    
    * fix dum speelig errror
    
    * Revise undervoltage value
    
    Co-authored-by: Joshua Feist <40469822+feistjo@users.noreply.github.com>
    
    * move minVoltage calculation to process state
    
    * added PR review suggestions
    
    * limit power output to 80 kW
    
    * fixed max power check in CalculateSOE
    
    ---------
    
    Co-authored-by: Joshua Feist <40469822+feistjo@users.noreply.github.com>

--- Firmware/PlatformIO/main_mcu/src/bms.cpp
+++ Firmware/PlatformIO/main_mcu/src/bms.cpp
@@ -1,5 +1,6 @@
 #include "bms.h"
 
+#include <numeric>
 #include <algorithm>
 
 int BMS::faultPin{-1};
@@ -17,6 +18,38 @@ void BMS::Tick(std::chrono::milliseconds elapsed_time)
 
     // log to SD, send to ESP, send to CAN
     // todo
+}
+
+// Find maximum discharge and regen current
+void BMS::CalculateSOE()
+{
+    float currentPerCell = current[0] / kNumCellsParallel;
+    float internalResistancePerSeriesElement = kInternalResistance / kNumCellsParallel;
+
+    // Find highest and lowest open circuit voltage
+    float minOpenCircuitVoltage = minVoltage + (currentPerCell * kInternalResistance);
+    float maxOpenCircuitVoltage = maxVoltage + (currentPerCell * kInternalResistance);
+
+    // Limit at V_open + I * (R_internal / numCellsParallel) = V_boundary
+    //  => I = (numCellsParallel / R_internal) * (V_boundary - V_open)
+    float maxDischargeVoltage = minOpenCircuitVoltage - kCellUndervoltage;
+    float maxRegenVoltage = kCellOvervoltage - maxOpenCircuitVoltage;
+    float uncappedDischargeCurrent = maxDischargeVoltage / internalResistancePerSeriesElement;
+    float uncappedRegenCurrent = maxRegenVoltage / internalResistancePerSeriesElement;
+
+    // I = P / V
+    float packVoltage = std::accumulate(voltages.begin(), voltages.end(), 0);
+    float powerCappedCurrent = kMaxPowerOutput / packVoltage;
+
+    maxDischargeCurrent = std::min({
+        uncappedDischargeCurrent,
+        powerCappedCurrent,
+        kDischargeCurrent
+    });
+    maxRegenCurrent = std::min(
+        uncappedRegenCurrent,
+        kRegenCurrent
+    );
 }
 
 void BMS::ProcessCooling()
@@ -48,6 +81,7 @@ void BMS::ProcessState()
             // check voltages
             bq_.GetVoltages(voltages);
             maxVoltage = *std::max_element(voltages.begin(), voltages.end());
+            minVoltage = *std::min_element(voltages.begin(), voltages.end());
             // send CAN messages with SOE (state of energy)
             break;
         case BMSState::kCharging:
