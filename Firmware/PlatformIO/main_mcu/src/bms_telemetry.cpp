6863da0bb965f6c437227b6eee4c2f5e3db8cb68
s2011r2593 <seanrhee2024@u.northwestern.edu>
2023-02-04 13:58:40 -0500

    initializes voltage/temperature signals and messages

--- Firmware/PlatformIO/main_mcu/src/bms_telemetry.cpp
+++ Firmware/PlatformIO/main_mcu/src/bms_telemetry.cpp
@@ -1,1 +1,49 @@
+// #include <stdio.h>
+
 #include "bms_telemetry.h"
+
+void BMSTelemetry::InitializeCAN()
+{
+  CreateVerboseMessages();
+}
+
+void BMSTelemetry::CreateVerboseMessages()
+{
+  for (int i = 0; i < kNumVoltageMessages; i++) {
+    voltage_signals_[i * kSignalsPerMessage + 0] = new MakeUnsignedCANSignal(float,  0, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 1] = new MakeUnsignedCANSignal(float,  8, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 2] = new MakeUnsignedCANSignal(float, 16, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 3] = new MakeUnsignedCANSignal(float, 24, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 4] = new MakeUnsignedCANSignal(float, 32, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 5] = new MakeUnsignedCANSignal(float, 40, 8, 0.012, 2);
+    voltage_signals_[i * kSignalsPerMessage + 6] = new MakeUnsignedCANSignal(float, 48, 8, 0.012, 2);
+
+    voltage_messages_[i] = new CANTXMessage<7>{vb_can_bus_, 512 + i, 7, kTransmitPeriod,
+      *voltage_signals_[i * kSignalsPerMessage + 0],
+      *voltage_signals_[i * kSignalsPerMessage + 1],
+      *voltage_signals_[i * kSignalsPerMessage + 2],
+      *voltage_signals_[i * kSignalsPerMessage + 3],
+      *voltage_signals_[i * kSignalsPerMessage + 4],
+      *voltage_signals_[i * kSignalsPerMessage + 5],
+      *voltage_signals_[i * kSignalsPerMessage + 6]};
+  }
+
+  for (int i = 0; i < kNumTemperatureMessages; i++) {
+    temperature_signals_[i * kSignalsPerMessage + 0] = new MakeUnsignedCANSignal(float,  0, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 1] = new MakeUnsignedCANSignal(float,  8, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 2] = new MakeUnsignedCANSignal(float, 16, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 3] = new MakeUnsignedCANSignal(float, 24, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 4] = new MakeUnsignedCANSignal(float, 32, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 5] = new MakeUnsignedCANSignal(float, 40, 8, 1, -40);
+    temperature_signals_[i * kSignalsPerMessage + 6] = new MakeUnsignedCANSignal(float, 48, 8, 1, -40);
+
+    temperature_messages_[i] = new CANTXMessage<7>{vb_can_bus_, 544 + i, 7, kTransmitPeriod,
+      *temperature_signals_[i * kSignalsPerMessage + 0],
+      *temperature_signals_[i * kSignalsPerMessage + 1],
+      *temperature_signals_[i * kSignalsPerMessage + 2],
+      *temperature_signals_[i * kSignalsPerMessage + 3],
+      *temperature_signals_[i * kSignalsPerMessage + 4],
+      *temperature_signals_[i * kSignalsPerMessage + 5],
+      *temperature_signals_[i * kSignalsPerMessage + 6]};
+  }
+}
--- Firmware/PlatformIO/main_mcu/src/bms_telemetry.h
+++ Firmware/PlatformIO/main_mcu/src/bms_telemetry.h
@@ -3,27 +3,44 @@
 #include "teensy_can.h"
 #include "virtualTimer.h"
 
-const int kHPCANBus = 1;
-const int kVerboseCANBus = 2;
-const int kLPCANBus = 3;
+const uint16_t kSignalsPerMessage = 7;
+const uint16_t kNumVoltageMessages = 20;
+const uint16_t kNumTemperatureMessages = 16;
+const int kTransmitPeriod = 10;
 
 class BMSTelemetry
 {
 public:
   BMSTelemetry(
-    TeensyCAN<kHPCANBus> hp_can_bus,
-    TeensyCAN<kVerboseCANBus> verbose_can_bus,
-    TeensyCAN<kLPCANBus> lp_can_bus
+    ICAN &hp_can_bus,
+    ICAN &vb_can_bus,
+    ICAN &lp_can_bus,
+    VirtualTimerGroup &timer_group
   )
     : hp_can_bus_{hp_can_bus},
-      verbose_can_bus_{verbose_can_bus},
-      lp_can_bus_{lp_can_bus}
+      vb_can_bus_{vb_can_bus},
+      lp_can_bus_{lp_can_bus},
+      timer_group_{timer_group}
   {
   }
-private:
-  TeensyCAN<kHPCANBus> hp_can_bus_;
-  TeensyCAN<kVerboseCANBus> verbose_can_bus_;
-  TeensyCAN<kLPCANBus> lp_can_bus_;
 
-  VirtualTimerGroup timer_group_{};
+  void InitializeCAN();
+private:
+  ICAN &hp_can_bus_;
+  ICAN &vb_can_bus_;
+  ICAN &lp_can_bus_;
+
+  VirtualTimerGroup timer_group_;
+
+  std::array<ICANSignal *, kSignalsPerMessage * kNumVoltageMessages> voltage_signals_;
+  std::array<ICANSignal *, kSignalsPerMessage * kNumTemperatureMessages> temperature_signals_;
+
+  std::array<CANTXMessage<kSignalsPerMessage> *, kNumVoltageMessages> voltage_messages_;
+  std::array<CANTXMessage<kSignalsPerMessage> *, kNumTemperatureMessages> temperature_messages_;
+  // single current message
+  
+  void CreateVerboseMessages();
 };
+
+// std array for messages
+// 
